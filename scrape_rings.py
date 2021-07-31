891902e80139b5262d0e0781899587bddef893ee
Phuong Nguyen <phngyn2@gmail.com>
2021-07-31 10:08:05 -0400

    Optimized using with loops

--- scrape_rings.py
+++ scrape_rings.py
@@ -1,129 +1,101 @@
 import os
-import bs4
-import urllib
 
 from urllib.request import urlopen
-from urllib.request import Request
 from bs4 import BeautifulSoup as bs
-from pprint import pprint
 
+BASE_URL = "https://www.idonowidont.com/marketplace/engagement-rings"
 
-container_posting_url = []
-#gets directory of current working file
 dir_path = os.path.dirname(os.path.realpath(__file__))
+ring_src = dir_path + "\\ring_src.txt"
+ring_data = dir_path + "\\ring_data.txt"
+# print(ring_src, ring_data)
 
-base_url = "https://www.idonowidont.com/marketplace/engagement-rings"
-ring_src = dir_path + "\ring_src.txt"
-ring_data = dir_path + "\ring_data.txt"
+#Find the last page on the website to know our loop range
 
-print(ring_src, ring_data)
+with urlopen(BASE_URL) as client:
+    page_html = client.read()
+    soup_html = bs(page_html, "html.parser")
+    last_page_url = soup_html.find("li", {"class":"pager__item pager__item--last"}).a["href"]
+    # not sure why last page is off by -1, so we add 1
+    last_page = int(last_page_url[-2:]) + 1
 
-#Find the last page to loop through
-uClient = urlopen(base_url)
-page_html = uClient.read()
-uClient.close()
-soup_html = bs(page_html, "html.parser")
-last_page_url = soup_html.find("li", {"class":"pager__item pager__item--last"}).a["href"] 
-
-# not sure why last page is off by -1, so we add 1
-last_page = int(last_page_url[-2:]) + 1
-
-
-# Loop through list of pages to gather all posting url
+# Loop through list of pages to gather all ring post url
 # and add url to our ring_src if it's not in the file
 for i in range(0, last_page + 1):
     if i == 0:
-        my_url = base_url
+        MY_URL = BASE_URL
     else:
-        my_url = base_url + "?page=" + str(i)
-    
-    print(my_url)
+        MY_URL = BASE_URL + "?page=" + str(i)
+    print(MY_URL)
 
-    #Opens the webpage, reads it into a container, then closes the client
-    uClient = urlopen(my_url)
-    page_html = uClient.read()
-    uClient.close()
-
-    #Uses soup to parse down to href to get weblink
-    soup_html = bs(page_html, "html.parser")
-    container_posting = soup_html.findAll("div", {"class":"ds-1col"})
+    with urlopen(MY_URL) as client:
+        page_html = client.read()
+        soup_html = bs(page_html, "html.parser")
+        container_posting = soup_html.findAll("div", {"class":"ds-1col"})
 
     for posts in container_posting:
         post_url = posts.div.a["href"]
         with open(ring_src, "r+") as file:
             if post_url in file.read():
-                pass
+                print( "Exists:", post_url)
             else:
                 file.write(post_url + "\n")
-
+                print( "Added:", post_url)
 
 # Read ring_src and check if link is valid
 # if valid, continue with data mine
 # if invalid, remove from ring_src
-
 with open(ring_src) as f:
     ring_src_lines = [line.rstrip() for line in f]
-    # print(ring_src_lines)
-
-with open(ring_data) as g:
-    ring_data_lines = [line.rstrip() for line in g]
-    # print(ring_data_lines)
 
 for url in ring_src_lines:
     target_url = "https://www.idonowidont.com" + url
-    # print(target_url)
 
     with open(ring_data) as data:
         if target_url in data.read():
-            pass
+            print( "Exists:", target_url)
         else:
             try:
-                page_html = client.read()
+                with urlopen(target_url) as client:
+                    page_html = client.read()
+                    soup_html = bs(page_html, "html.parser")
+
+                    try:
+                        container_price = soup_html.find("span", {"class":"product-price"})
+                        price = container_price.get_text()
+                    except:
+                        price = "null"
+
+                    try:
+                        container_carat = soup_html.find("li", {"class":"field_carat_weight"})
+                        carat = container_carat.div.div.get_text()
+                    except:
+                        carat = "null"
+
+                    try:
+                        container_clarity = soup_html.find("li", {"class":"field_clarity"})
+                        clarity = container_clarity.div.div.get_text()
+                    except:
+                        clarity = "null"
+
+                    try:
+                        container_shape = soup_html.find("li", {"class":"field_shape"})
+                        shape = container_shape.div.div.get_text()
+                    except:
+                        shape = "null"
+
+                    try:
+                        container_color = soup_html.find("li", {"class":"field_color"})
+                        color = container_color.div.div.get_text()
+                    except:
+                        color = "null"
+
+                    values = target_url + "|" + price + "|" + carat + "|" + clarity + "|" + shape + "|" + color
+
+                    with open(ring_data, "a") as data:
+                        data.write(values + "\n")
+                        print("Added:", values)
             except:
                 pass
 
-    soup_html = bs(page_html, "html.parser")
-
-    try:
-        #gets price
-        container_price = soup_html.find("span", {"class":"product-price"})
-        price = container_price.get_text()
-    except:
-        pass
-
-    try:
-        #gets carat
-        container_carat = soup_html.find("li", {"class":"field_carat_weight"})
-        carat = container_carat.div.div.get_text()
-    except:
-        pass
     
-    try:
-        #gets clarity
-        container_clarity = soup_html.find("li", {"class":"field_clarity"})
-        clarity = container_clarity.div.div.get_text()
-    except:
-        pass
-
-    try:
-        #gets shape
-        container_shape = soup_html.find("li", {"class":"field_shape"})
-        shape = container_shape.div.div.get_text()
-    except:
-        pass
-
-    try:
-        #gets color
-        container_color = soup_html.find("li", {"class":"field_color"})
-        color = container_color.div.div.get_text()
-    except:
-        pass
-
-    # values = []
-    # values = [target_url, price, carat, clarity, shape, color]
-    values = target_url + "|" + price + "|" + carat + "|" + clarity + "|" + shape + "|" + color
-
-    textfile = open(ring_data, "a")
-    textfile.write(values + "\n")
-    textfile.close()
-
