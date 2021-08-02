274edac9ae3306f1993de891af32fa9596e1c90d
Phuong Nguyen <phngyn2@gmail.com>
2021-08-02 09:58:21 -0400

    Using pandas to parse data

---
+++ pd_reader.py
@@ -0,0 +1,18 @@
+from datetime import date
+import os
+import json
+import pprint
+import pandas as pd
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+ring_data = dir_path + "\\ring_data.json"
+
+data = []
+
+with open(ring_data) as rdata:
+    for line in rdata:
+        data.append(json.loads(line))
+
+df = pd.DataFrame(data).to_clipboard()
+
+# print(df)
